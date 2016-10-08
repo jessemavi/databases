675374c692ab9dfa7f878a40ec6a448ddcd65886
jessemavi <jesse.mavi@gmail.com>
2016-10-08 10:44:03 -0700

    Implement SQL table-creation script. Add files for common SQL commands and a batch script in case we need it.

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,30 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE rooms (
+  id int NOT NULL AUTO_INCREMENT,
+  roomname varchar(255),
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  id int NOT NULL AUTO_INCREMENT,
+  text varchar(255),
+  createdAt DATETIME NOT NULL DEFAULT NOW(),
+  user_id int,
+  room_id int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) references users(id), -- shows up as MUL in schema
+  FOREIGN KEY (room_id) references rooms(id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
